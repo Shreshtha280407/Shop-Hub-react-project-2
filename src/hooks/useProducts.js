195b36dd3519f2219ca837e8b959555194890e35
Shreshtha280407 <shreshthashreshtha28@gmail.com>
2026-03-25 01:17:30 +0530

    Refactor useProducts to use API service
    Refactor useProducts hook to use getAllProducts API service.

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,29 +1,3 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-
-// function useProducts() {
-//   const [products, setProducts] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     axios
-//       .get("https://fakestoreapi.com/products")
-//       .then((res) => {
-//         setProducts(res.data);
-//         setLoading(false);
-//       })
-//       .catch((err) => {
-//         setError(err);
-//         setLoading(false);
-//       });
-//   }, []);
-
-//   return { products, loading, error };
-// }
-
-// export default useProducts;
-
 import { useEffect, useState } from "react";
 import { getAllProducts } from "../services/api";
 
